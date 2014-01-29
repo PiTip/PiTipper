b99de06092afb563c12daaa88274bd57d92f699c
Graham Higgins <gjh-github@bel-epa.com>
2014-01-29 05:04:57 +0000

    Redo test

--- tests/test_pitipper.py
+++ tests/test_pitipper.py
@@ -1,7 +1,11 @@
+"""
+Needs to be able to explore completely the subreddit "forest".
+It's a graph, ofc.
+"""
+
 import unittest
 import logging
 import praw
-import itertools
 
 log = logging.getLogger(__file__)
 log.setLevel(logging.DEBUG)
@@ -13,9 +17,6 @@ log.addHandler(handler)
 
 class TestUpdate(unittest.TestCase):
     def setUp(self):
-        # import configparser
-        # config = configparser.ConfigParser()
-        # config.read('example.ini')
         import os
         try:
             (self.user, self.password) = os.environ.get(
@@ -29,8 +30,8 @@ class TestUpdate(unittest.TestCase):
     def tearDown(self):
         pass
 
-    # @unittest.skip("Passed, skipping")
-    def test_do_prawtest(self):
+    @unittest.skip("Passed, skipping")
+    def test_do_itertest(self):
         r = praw.Reddit(
             'Pi DogeTipBot by /u/pitipper aka JC300. Source code is available.'
             )
@@ -38,9 +39,7 @@ class TestUpdate(unittest.TestCase):
         r.login(self.user, self.password)
         already_done = set()
         # Look for recent submissions in r/dogecoin
-        for comment in itertools.islice(
-                r.get_subreddit('dogecoin'
-                    ).get_comments(), 10):
+        for comment in r.get_subreddit('dogecoin').get_comments(limit=10):
             log.debug("Doing comment id {0}".format(comment.id))
             for prawobj in [comment.submission.comments]:
                 subcomments = [prawobj] \
@@ -52,6 +51,38 @@ class TestUpdate(unittest.TestCase):
                                   "TipBot 31.415926 doge')".format(
                                 subcomment.id))
                         already_done.add(subcomment.id)
+
+    @unittest.skip("Passed, skipping")
+    def test_do_flatten_test(self):
+        r = praw.Reddit(
+            'Pi DogeTipBot by /u/pitipper aka JC300. Source code is available.'
+            )
+        r.login(self.user, self.password)
+        already_done = set()
+        # Look for recent submissions in r/dogecoin
+        for comment in r.get_subreddit('dogecoin').get_comments(limit=6):
+            log.debug("Doing comment id {0}".format(comment.id))
+            for fcomment in praw.helpers.flatten_tree(
+                    comment.submission.comments):
+                # Not as flat as one might have expected
+                if isinstance(fcomment, praw.objects.Comment):
+                    if fcomment.body == "Tip" \
+                        and fcomment.id not in already_done:
+                        log.debug("Reply to comment {0} ('+/u/Doge" + \
+                                  "TipBot 31.415926 doge')".format(
+                                fcomment.id))
+                        already_done.add(fcomment.id)
+                elif isinstance(fcomment, praw.objects.MoreComments):
+                    log.debug("Handle MoreComments")
+                    # make repeated API calls to reddit
+                    for scomment in fcomment.comments():
+                        log.debug("Doing scomment id {0}".format(scomment.id))
+                        if scomment.body == "Tip" \
+                            and scomment.id not in already_done:
+                            log.debug("Reply to comment {0} ('+/u/Doge" + \
+                                      "TipBot 31.415926 doge')".format(
+                                    scomment.id))
+                            already_done.add(scomment.id)
 
 
 if __name__ == '__main__':
